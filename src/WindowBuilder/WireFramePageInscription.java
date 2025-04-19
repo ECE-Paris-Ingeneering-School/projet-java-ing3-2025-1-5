e30018a591553707c3007adbe3ff404963df24c9
Felix CADENE <felixcadene0@gmail.com>
2025-04-19 12:25:24 +0200

    Succes -> Inscription d'un client via la page d'Inscription -> Ajout dans la BDD les coord. Modif de l'interface graphique des pages connexion et inscription

--- src/WindowBuilder/WireFramePageInscription.java
+++ src/WindowBuilder/WireFramePageInscription.java
@@ -1,5 +1,6 @@
 package WindowBuilder;
 
+import MVC.modele.Client;
 import WindowBuilder.helper_classes.*;
 import dao.*;
 import java.awt.*;
@@ -32,7 +33,7 @@ public class WireFramePageInscription {
      panel.add(element42);
 
      JPasswordField element44 = new JPasswordField("");
-     element44.setBounds(306, 155, 165, 24);
+     element44.setBounds(306, 155, 165, 36);
      element44.setFont(CustomFontLoader.loadFont("./resources/fonts/Lexend.ttf", 24));
      element44.setBackground(Color.decode("#ffe7bf"));
      element44.setForeground(Color.decode("#73664e"));
@@ -43,7 +44,7 @@ public class WireFramePageInscription {
      
 
      JTextField element48 = new JTextField("");
-     element48.setBounds(306, 201, 165, 24);
+     element48.setBounds(306, 201, 165, 36);
      element48.setFont(CustomFontLoader.loadFont("./resources/fonts/Lexend.ttf", 24));
      element48.setBackground(Color.decode("#ffe7bf"));
      element48.setForeground(Color.decode("#73664e"));
@@ -51,8 +52,17 @@ public class WireFramePageInscription {
      OnFocusEventHelper.setOnFocusText(element48, "Mail", Color.decode("#000"),   Color.decode("#73664e"));
      panel.add(element48);
 
+     JTextField element49 = new JTextField("");
+     element49.setBounds(306, 248, 165, 36);
+     element49.setFont(CustomFontLoader.loadFont("./resources/fonts/Lexend.ttf", 24));
+     element49.setBackground(Color.decode("#ffe7bf"));
+     element49.setForeground(Color.decode("#73664e"));
+     element49.setBorder(new RoundedBorder(2, Color.decode("#000"), 1));
+     OnFocusEventHelper.setOnFocusText(element49, "Telephone", Color.decode("#000"),   Color.decode("#73664e"));
+     panel.add(element49);
+
      JButton element45 = new JButton("Inscription");
-     element45.setBounds(307, 258, 167, 44);
+     element45.setBounds(307, 300, 167, 44);
      element45.setBackground(Color.decode("#bca8e4"));
      element45.setForeground(Color.decode("#000"));
      element45.setFont(CustomFontLoader.loadFont("./resources/fonts/Lexend.ttf", 24));
@@ -63,34 +73,54 @@ public class WireFramePageInscription {
           public void actionPerformed(java.awt.event.ActionEvent evt) {
             String nom = element42.getText();
             String mail = element48.getText();
+            String telephone = element49.getText();
             String mdp = new String(element44.getPassword());
-            if (nom.isEmpty() || mail.isEmpty() || mdp.isEmpty() || nom.equals("Nom") || mail.equals("Mail") || mail.equals("Mot de passe")) {
+            if (nom.isEmpty() || mail.isEmpty() || mdp.isEmpty() || telephone.isEmpty() || nom.equals("Nom") || mail.equals("Mail") || mail.equals("Mot de passe") || telephone.equals("Telephone")) {
                 JOptionPane.showMessageDialog(frame, "Veuillez remplir tous les champs.", "Erreur", JOptionPane.ERROR_MESSAGE);
-            } else {
+            } 
+            // Conditions pour vérifier si le mail est valide -> regarder si il contient un @.
+            else if (!mail.contains("@") || !mail.contains(".")) {
+                JOptionPane.showMessageDialog(frame, "Veuillez entrer un email valide.", "Erreur", JOptionPane.ERROR_MESSAGE);
+            } 
+            // Conditions pour verifier si le telephone est valide -> regarder si il contient 10 chiffres (10 int)
+            else if (telephone.length() != 10 || !telephone.matches("[0-9]+")) {
+                JOptionPane.showMessageDialog(frame, "Veuillez entrer un numéro de téléphone valide.", "Erreur", JOptionPane.ERROR_MESSAGE);
+            }    
+            // Conditions respectees --> On peut passer a l'inscription
+            else {
                 daoConnect dao = daoConnect.getInstance("wherebnb", "root", "");
                 daoClient clientDAO = new daoClient(dao);
-                //Client newClient = new Client(nom, mail, mdp);
                 if (clientDAO.existe(mail) == true) {
                     JOptionPane.showMessageDialog(frame, "Cet utilisateur existe déjà.", "Erreur", JOptionPane.ERROR_MESSAGE);
                     return;
                 }
-                //clientDAO.create(newClient);
-                JOptionPane.showMessageDialog(frame, "Inscription réussie !", "Succès", JOptionPane.INFORMATION_MESSAGE);
-            }
+                
+                // Créer un objet Client
+                Client nouveauClient = new Client(0,nom, mail,telephone, mdp, false,false);
 
+                // Ajouter à la base
+                int idAjoute = clientDAO.ajouter(nouveauClient);
+
+                if (idAjoute != 0) {
+                    JOptionPane.showMessageDialog(frame, "Inscription réussie !", "Succès", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    //l'ajout echoue si idAjoute == 0 car l'id n'est pas auto-incrementé
+                    JOptionPane.showMessageDialog(frame, "Erreur lors de l'inscription.", "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
+            }
           }
       });
      panel.add(element45);
 
-     JButton element49 = new JButton("Contactez nous");
-     element49.setBounds(614, 337, 141, 35);
-     element49.setBackground(Color.decode("#bca8e4"));
-     element49.setForeground(Color.decode("#000"));
-     element49.setFont(CustomFontLoader.loadFont("./resources/fonts/Lexend.ttf", 14));
-     element49.setBorder(new RoundedBorder(4, Color.decode("#3d364a"), 1));
-     element49.setFocusPainted(false);
-     OnClickEventHelper.setOnClickColor(element49, Color.decode("#7c6f97"), Color.decode("#bca8e4"));
-     panel.add(element49);
+     JButton element50 = new JButton("Contactez nous");
+     element50.setBounds(614, 337, 141, 35);
+     element50.setBackground(Color.decode("#bca8e4"));
+     element50.setForeground(Color.decode("#000"));
+     element50.setFont(CustomFontLoader.loadFont("./resources/fonts/Lexend.ttf", 14));
+     element50.setBorder(new RoundedBorder(4, Color.decode("#3d364a"), 1));
+     element50.setFocusPainted(false);
+     OnClickEventHelper.setOnClickColor(element50, Color.decode("#7c6f97"), Color.decode("#bca8e4"));
+     panel.add(element50);
 
 
      frame.add(panel);
