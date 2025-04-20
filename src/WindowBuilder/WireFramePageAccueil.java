4c5ee3c8067d370136f1b92723699689f4f50cf2
Felix CADENE <felixcadene0@gmail.com>
2025-04-20 20:12:39 +0200

    Finalisation du wireframe Accueil avec le bouton de recherche - Modif du DAO Logement pour implementer la rechercher via des verifs + SQL. Implementation avec succes

--- src/WindowBuilder/WireFramePageAccueil.java
+++ src/WindowBuilder/WireFramePageAccueil.java
@@ -1,7 +1,13 @@
 package WindowBuilder;
 
+import MVC.modele.Logement;
 import WindowBuilder.helper_classes.*;
+import dao.daoConnect;
+import dao.daoLogement;
 import java.awt.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import javax.swing.*;
 
 public class WireFramePageAccueil {
@@ -46,7 +52,7 @@ public class WireFramePageAccueil {
       Navig_Bar.add(element4);
 
       ImageIcon emojiIcon = scaleIcon("src/ressources/emojis/hug.png", 20, 20);
-      JButton element5 = new JButton("Votre compte", emojiIcon);
+      JButton element5 = new JButton("Mon compte", emojiIcon);
       element5.setBounds(600, 6, 150, 40);
       element5.setBackground(Color.decode("#bca8e4"));
       element5.setForeground(Color.decode("#000000"));
@@ -70,11 +76,29 @@ public class WireFramePageAccueil {
       element7.setForeground(Color.decode("#000"));
       panel.add(element7);
 
-      // Icon-based Input Fields
-      panel.add(merge_icon_Text("O\u00f9 allez-vous ?", "src/ressources/emojis/localisation.png", 59, 195, 137, 24));
-      panel.add(merge_icon_Text("Date d'arriv\u00e9e", "src/ressources/emojis/arrivee.png", 216, 195, 124, 24));
-      panel.add(merge_icon_Text("Date de d\u00e9part", "src/ressources/emojis/depart.png", 346, 195, 124, 24));
-      panel.add(merge_icon_Text("Personnes", "src/ressources/emojis/person.png", 483, 195, 119, 24));
+      JTextField element_localisation = new JTextField();
+      JTextField element_arrivee = new JTextField();
+      JTextField element_depart = new JTextField();
+      JTextField element_personnes = new JTextField();
+      panel.add(merge_icon_Text("O\u00f9 allez-vous ?", "src/ressources/emojis/localisation.png", element_localisation, 59, 195, 137, 24));
+      panel.add(merge_icon_Text("Date d'arriv\u00e9e", "src/ressources/emojis/arrivee.png", element_arrivee, 216, 195, 124, 24));
+      panel.add(merge_icon_Text("Date de d\u00e9part", "src/ressources/emojis/depart.png", element_depart, 346, 195, 124, 24));
+      //panel.add(merge_Icon_text_Date("📅 Date d'arrivée", "src/ressources/emojis/arrivee.png", 216, 195, 124, 24));
+      //panel.add(merge_Icon_text_Date("📅 Date de départ", "src/ressources/emojis/depart.png", 346, 195, 124, 24));
+
+      panel.add(merge_icon_Text("Personnes", "src/ressources/emojis/person.png", element_personnes, 483, 195, 119, 24));
+
+      JLabel element8 = new JLabel("Format: jj/mm/aaaa");
+      element8.setBounds(216, 220, 124, 24);
+      element8.setFont(CustomFontLoader.loadFont("./resources/fonts/Lexend.ttf", 10));
+      element8.setForeground(Color.decode("#000"));
+      panel.add(element8);
+
+      JLabel element9 = new JLabel("Format: jj/mm/aaaa");
+      element9.setBounds(346, 220, 124, 24);
+      element9.setFont(CustomFontLoader.loadFont("./resources/fonts/Lexend.ttf", 10));
+      element9.setForeground(Color.decode("#000"));
+      panel.add(element9);
       
 
       JButton element11 = new JButton("Rechercher");
@@ -85,6 +109,106 @@ public class WireFramePageAccueil {
       element11.setBorder(new RoundedBorder(4, Color.decode("#3d364a"), 1));
       element11.setFocusPainted(false);
       OnClickEventHelper.setOnClickColor(element11, Color.decode("#7c6f97"), Color.decode("#bca8e4"));
+      element11.addActionListener(e -> {
+      String localisation = element_localisation.getText().trim();
+      String arrivee = element_arrivee.getText().trim();
+      String depart = element_depart.getText().trim();
+      String nbPersonnes = element_personnes.getText().trim();
+
+      if (localisation.isEmpty()) {
+         localisation = "";
+      } if (arrivee.isEmpty()) {
+         arrivee = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+      } if (depart.isEmpty()) {
+         depart = LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+      } if (nbPersonnes.isEmpty()) {
+         nbPersonnes = "1";
+      }
+
+      /*
+      * VERIFICATION DE LA LOCALISATION:
+      * --> La localisation ne doit pas contenir de chiffres/caractères spéciaux
+      */
+      for (char c : localisation.toCharArray()) {
+        if (Character.isDigit(c)) {
+            JOptionPane.showMessageDialog(frame, "La localisation ne doit pas contenir de chiffres.");
+            return;
+        }
+      }
+
+
+      /*
+      * VERIFICATION DU NOMBRE DE PERSONNES:
+      * --> Le nombre de personnes doit être supérieur à 0
+      * --> Le nombre de personnes doit être un nombre entier
+      */
+
+      int nbPersonnesInt = Integer.parseInt(nbPersonnes);
+      if (nbPersonnesInt < 1) { 
+         JOptionPane.showMessageDialog(frame, "Le nombre de personnes doit être supérieur à 0.");
+         return;
+      }
+
+      try {
+         Integer.parseInt(nbPersonnes);
+      } catch (NumberFormatException ex) {
+         JOptionPane.showMessageDialog(frame, "Le nombre de personnes doit être un nombre entier.");
+         return;
+      }
+
+      
+      /*
+      * VERIFICATION DES DATES:
+      * --> La date d'arrivée doit être au format jj/mm/aaaa
+      * --> La date de départ doit être au format jj/mm/aaaa
+      * --> La date d'arrivée et de départ ne peuvent pas être identiques
+      * --> La date de départ doit être après la date d'arrivée
+      * --> La date d'arrivée et de départ ne doivent pas être dans le passé
+      */
+      DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+      try {
+         LocalDate.parse(arrivee, dtf);
+      } catch (Exception ex) {
+         JOptionPane.showMessageDialog(frame, "La date d'arrivée doit être au format jj/mm/aaaa.");
+         return;
+      }
+      try {
+         LocalDate.parse(depart, dtf);
+      } catch (Exception ex) {
+         JOptionPane.showMessageDialog(frame, "La date de départ doit être au format jj/mm/aaaa.");
+         return;
+      }
+
+      if (arrivee.equals(depart)) {
+         JOptionPane.showMessageDialog(frame, "La date d'arrivée et de départ ne peuvent pas être identiques.");
+         return;
+      }
+
+      LocalDate dateArrivee = LocalDate.parse(arrivee, dtf);
+      LocalDate dateDepart = LocalDate.parse(depart, dtf);
+      if (dateDepart.isBefore(dateArrivee)) {
+         JOptionPane.showMessageDialog(frame, "La date de départ doit être après la date d'arrivée.");
+         return;
+      }
+
+      LocalDate today = LocalDate.now();
+      if (dateArrivee.isBefore(today) || dateDepart.isBefore(today)) {
+         JOptionPane.showMessageDialog(frame, "La date d'arrivée et de départ ne doivent pas être dans le passé.");
+         return;
+      }
+
+
+      daoConnect dao = daoConnect.getInstance("wherebnb", "root", "");
+      daoLogement daoLogementInstance = new daoLogement(dao);
+      List<Logement> resultats = daoLogementInstance.rechercher(localisation, dateArrivee, dateDepart, nbPersonnesInt);
+      if (resultats.isEmpty()) {
+         JOptionPane.showMessageDialog(frame, "Aucun logement trouvé pour les critères donnés.");
+      } else {
+         JOptionPane.showMessageDialog(frame, resultats.size() + " logements trouvés.");
+      }
+
+});
+
       panel.add(element11);
 
 
@@ -99,34 +223,38 @@ public class WireFramePageAccueil {
       element16.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Contactez nous"));
       panel.add(element16);
 
+
       frame.add(panel);
       frame.setVisible(true);
    }
 
-   private static JPanel merge_icon_Text(String txt_defaut, String iconPath, int x, int y, int width, int height) {
+   private static JPanel merge_icon_Text(String txt_defaut, String iconPath, JTextField textField, int x, int y, int width, int height) {
       JPanel merger = new JPanel();
       merger.setLayout(new BorderLayout(5, 0));
       merger.setBounds(x, y, width, height);
       merger.setBackground(Color.decode("#ffe7bf"));
       merger.setBorder(new RoundedBorder(2, Color.decode("#000"), 1));
-
+  
       JLabel iconLabel = new JLabel(scaleIcon(iconPath, 18, 18));
       iconLabel.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
       merger.add(iconLabel, BorderLayout.WEST);
-
-      JTextField textField = new JTextField(txt_defaut);
+  
+      // Use the textField passed in
+      textField.setText(txt_defaut);
       textField.setFont(CustomFontLoader.loadFont("./resources/fonts/Lexend.ttf", 14));
       textField.setBackground(Color.decode("#ffe7bf"));
       textField.setForeground(Color.decode("#73664e"));
       textField.setBorder(null);
       textField.setOpaque(true);
       textField.setCaretColor(Color.BLACK);
-
+  
       OnFocusEventHelper.setOnFocusText(textField, txt_defaut, Color.BLACK, Color.decode("#73664e"));
-
+  
       merger.add(textField, BorderLayout.CENTER);
       return merger;
-   }
+  }
+  
+
 
    private static ImageIcon scaleIcon(String path, int width, int height) {
       ImageIcon icon = new ImageIcon(path);
--- src/dao/daoLogement.java
+++ src/dao/daoLogement.java
@@ -1,9 +1,10 @@
 package dao;
 
 import MVC.modele.Logement;
-
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Implémentation MySQL pour la gestion des logements dans la base de données.
@@ -64,7 +65,7 @@ public class daoLogement implements daoInterface<Logement> {
         int id = 0;
         String query = "INSERT INTO LOGEMENT (Nom, Prix, Description, Liste_photos, Proprio_ID, Adresse_ID) VALUES (?, ?, ?, ?, ?, ?)";
         try (Connection connexion = daoConnect.getConnection();
-             PreparedStatement preparedStatement = connexion.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            PreparedStatement preparedStatement = connexion.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
 
             preparedStatement.setString(1, logement.getNom());
             preparedStatement.setFloat(2, logement.getPrix());
@@ -90,7 +91,7 @@ public class daoLogement implements daoInterface<Logement> {
         Logement logement = null;
         String query = "SELECT * FROM LOGEMENT WHERE Logement_ID = ?";
         try (Connection connexion = daoConnect.getConnection();
-             PreparedStatement preparedStatement = connexion.prepareStatement(query)) {
+            PreparedStatement preparedStatement = connexion.prepareStatement(query)) {
 
             preparedStatement.setInt(1, id);
             ResultSet resultats = preparedStatement.executeQuery();
@@ -117,7 +118,7 @@ public class daoLogement implements daoInterface<Logement> {
     public Logement modifier(Logement logement) {
         String query = "UPDATE LOGEMENT SET Nom = ?, Prix = ?, Description = ?, Liste_photos = ?, Proprio_ID = ?, Adresse_ID = ? WHERE Logement_ID = ?";
         try (Connection connexion = daoConnect.getConnection();
-             PreparedStatement preparedStatement = connexion.prepareStatement(query)) {
+            PreparedStatement preparedStatement = connexion.prepareStatement(query)) {
 
             preparedStatement.setString(1, logement.getNom());
             preparedStatement.setFloat(2, logement.getPrix());
@@ -139,7 +140,7 @@ public class daoLogement implements daoInterface<Logement> {
     public Logement supprimer(Logement logement) {
         String query = "DELETE FROM LOGEMENT WHERE Logement_ID = ?";
         try (Connection connexion = daoConnect.getConnection();
-             PreparedStatement preparedStatement = connexion.prepareStatement(query)) {
+            PreparedStatement preparedStatement = connexion.prepareStatement(query)) {
 
             preparedStatement.setInt(1, logement.getLogementId());
             preparedStatement.executeUpdate();
@@ -148,5 +149,57 @@ public class daoLogement implements daoInterface<Logement> {
             System.out.println("Erreur lors de la suppression du logement.");
         }
         return logement;
+    }
+
+    /**
+     * Recherche des logements disponibles dans une ville donnée pour une période spécifique.
+     * @param ville La ville où rechercher les logements.
+     * @param dateArrivee La date d'arrivée.
+     * @param dateDepart La date de départ.
+     * @param nbPersonnes Le nombre de personnes.
+     * @return Une liste de logements disponibles correspondant aux critères de recherche.
+     */
+    public List<Logement> rechercher(String ville, LocalDate dateArrivee, LocalDate dateDepart, int nbPersonnes) {
+        List <Logement> logements = new ArrayList<>();
+
+        try {
+            Connection connexion_dao = daoConnect.getConnection();
+            if (ville.equals("")) {
+                ville = "%";
+            }
+            
+            String query = 
+            "SELECT * FROM LOGEMENT l " +
+            "JOIN adresse a ON l.Adresse_ID = a.Adresse_ID " +
+            "JOIN logement_general lg ON l.logement_id = lg.Logement_ID " +
+            "JOIN options_communes oc ON lg.Logement_ID = oc.Logement_ID " +
+            "WHERE a.Ville LIKE ? AND oc.Nb_personnes >= ? " +
+            "AND l.Logement_ID NOT IN ( " +
+                "SELECT Logement_ID FROM RESERVATION " +
+                "WHERE (Date_arrivee <= ? AND Date_depart >= ?) OR (Date_arrivee <= ? AND Date_depart >= ?) " +
+            ")";
+            PreparedStatement preparedStatement = connexion_dao.prepareStatement(query);
+            preparedStatement.setString(1, "%" + ville + "%");
+            preparedStatement.setInt(2, nbPersonnes);
+            ResultSet resultats = preparedStatement.executeQuery();
+
+            while (resultats.next()) {
+                Logement logement = new Logement(
+                    resultats.getInt("Logement_ID"),
+                    resultats.getString("Nom"),
+                    resultats.getString("Adresse"),
+                    resultats.getString("GeoCoord"),
+                    resultats.getFloat("Prix"),
+                    resultats.getString("Description"),
+                    resultats.getString("Liste_photos"),
+                    resultats.getInt("Proprio_ID")
+                );
+                logements.add(logement);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erreur lors de la recherche de logements disponibles.");
+        }
+        return logements;
     }
 }
