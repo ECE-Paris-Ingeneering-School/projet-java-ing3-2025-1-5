2d5e77b766f3e77ea0f2321e7a709711438f1fff
Léon <git.staleness589@passmail.com>
2025-04-20 23:42:12 +0200

    🆕 Modèle des options

--- src/MVC/modele/OptionsHotel.java
+++ src/MVC/modele/OptionsHotel.java
@@ -1,5 +1,29 @@
 package MVC.modele;
 
 public class OptionsHotel {
-    
+    private int optionsId;
+    private boolean petitDejeuner;
+    private boolean bar;
+    private boolean salleDeSport;
+    private boolean serviceEtage;
+    private int nbEtoiles;
+
+    // Getters et Setters
+    public int getOptionsId() { return optionsId; }
+    public void setOptionsId(int optionsId) { this.optionsId = optionsId; }
+
+    public boolean isPetitDejeuner() { return petitDejeuner; }
+    public void setPetitDejeuner(boolean petitDejeuner) { this.petitDejeuner = petitDejeuner; }
+
+    public boolean isBar() { return bar; }
+    public void setBar(boolean bar) { this.bar = bar; }
+
+    public boolean isSalleDeSport() { return salleDeSport; }
+    public void setSalleDeSport(boolean salleDeSport) { this.salleDeSport = salleDeSport; }
+
+    public boolean isServiceEtage() { return serviceEtage; }
+    public void setServiceEtage(boolean serviceEtage) { this.serviceEtage = serviceEtage; }
+
+    public int getNbEtoiles() { return nbEtoiles; }
+    public void setNbEtoiles(int nbEtoiles) { this.nbEtoiles = nbEtoiles; }
 }
